39c03f05ebc555f1d42bfbf8fa9e72d6f4a71953
Theodore <theodoreyip19@gmail.com>
2018-10-30 16:19:07 +0100

    display question, check answer

--- src/views/Category/CategoryContainer.js
+++ src/views/Category/CategoryContainer.js
@@ -4,9 +4,16 @@ import Category from './Category'
 import storeLocal from '../../helpers/localstorage'
 
 class CategoryContainer extends Component {
-  state = {
-    category: {}
+  constructor (props) {
+    super(props)
+    this.state = {
+      category: {},
+      input: ''
+    }
+    this.inputChange = this.inputChange.bind(this)
+    this.checkAnswer = this.checkAnswer.bind(this)
   }
+  
   componentWillMount () {
     storeLocal.saveCategory(this.props.match.params.id)
     console.log(storeLocal.getCategory())
@@ -17,9 +24,35 @@ class CategoryContainer extends Component {
       category: data
     })
   }
+  
+  inputChange (e) {
+    this.setState({
+      input: e.target.value
+    })
+  }
+
+  checkAnswer (e) {
+    if (e.which !== 13) return
+    const { category: { clues } } = this.state
+
+    console.log('CHECK >', e.target.value)
+    console.log('answer = ', clues[0].answer)
+    if(this.state.input.toLowerCase() === clues[0].answer.toLowerCase() ){
+      console.log('gagné')
+    } else {
+      console.log('prouuuuut')
+    }
+  }
+
   render () {
+    const { category, input } = this.state
     return (
-      <Category category={this.state.category} />
+      <Category
+        category={category}
+        input={input}
+        inputChange={this.inputChange}
+        checkAnswer={this.checkAnswer}
+      />
     )
   }
 }
