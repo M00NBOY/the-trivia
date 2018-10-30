3c1870dd5691a77d946301417987d05cc99bfd84
Mahel <zeroualmahel@gmail.com>
2018-10-30 14:07:25 +0100

    add localstorage helper + category'

--- src/views/Category/CategoryContainer.js
+++ src/views/Category/CategoryContainer.js
@@ -1,10 +1,15 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import api from '../../helpers/api'
 import Category from './Category'
+import storeLocal from '../../helpers/localstorage'
 
 class CategoryContainer extends Component {
   state = {
     category: {}
+  }
+  componentWillMount () {
+    storeLocal.saveCategory(this.props.match.params.name)
+    console.log(storeLocal.getCategory())
   }
   async componentDidMount () {
     const data = await api.getCategorybyId(this.props.match.params.name)
