23ba801512e325ccc3a38a8fb921b252edeb8135
Vincent Pham <thanh-son-vincent.pham@hetic.net>
2018-11-03 15:57:45 +0100

    old style

--- src/views/Category/Category.js
+++ src/views/Category/Category.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Category = ({
   category: { clues, title },
@@ -6,16 +7,25 @@ const Category = ({
   inputChange,
   checkAnswer
 }) => (
-  <section>
-    <h3>Category page : {title}</h3>
-    <h2>{clues && clues[0].question}</h2>
-    
-    <input
-      type='text'
-      value={input}
-      onChange={inputChange}
-      onKeyDown={checkAnswer}
-    />
+  <section className="category">
+    <Link className ="back" to="">Back to Home</Link>
+    <h3>Category: {title}</h3>
+    <div className="question">
+      <h4>Question:</h4>
+      <p>{clues && clues[0].question}</p>
+    </div>
+    <div className="answer">
+      <p>Your answer:</p>
+      <input
+        className="answerInput"
+        type='text'
+        value={input}
+        onChange={inputChange}
+        onKeyDown={checkAnswer}
+      />
+      <span className="answerDisplay">{input}</span>
+
+    </div>
     
   </section>
 )
