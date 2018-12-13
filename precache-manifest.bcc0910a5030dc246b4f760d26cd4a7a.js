c8b714e9041e997ce181b082ca9b32953b0f2c6f
Vincent Pham <thanh-son-vincent.pham@hetic.net>
2018-12-13 17:48:20 +0100

    deploy

---
+++ precache-manifest.bcc0910a5030dc246b4f760d26cd4a7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e21aa928b04045c2183c",
+    "url": "/the-trivia/static/css/main.9ddf1710.chunk.css"
+  },
+  {
+    "revision": "e21aa928b04045c2183c",
+    "url": "/the-trivia/static/js/main.e21aa928.chunk.js"
+  },
+  {
+    "revision": "5cf12f14e856446718c4",
+    "url": "/the-trivia/static/js/1.5cf12f14.chunk.js"
+  },
+  {
+    "revision": "48357aad3ef3f69b06d6",
+    "url": "/the-trivia/static/js/runtime~main.48357aad.js"
+  },
+  {
+    "revision": "d55abc3c7d1a4719dea9fa15ccec203f",
+    "url": "/the-trivia/static/media/VT323-Regular.d55abc3c.ttf"
+  },
+  {
+    "revision": "849ad03619e064bc108ec68e155e9ff4",
+    "url": "/the-trivia/index.html"
+  }
+];
