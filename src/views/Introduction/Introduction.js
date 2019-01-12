be0c01d9e648d60fe106307972633eb65ea4a1ff
Vincent Pham <thanh-son-vincent.pham@hetic.net>
2019-01-12 23:47:57 +0100

    update intro page

--- src/views/Introduction/Introduction.js
+++ src/views/Introduction/Introduction.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 
-const text = 'Click me to start!'
+const text = 'Click here to start!'
 
 class Introduction extends Component {
   constructor (props) {
     super(props)
     this.state = {
       count: 0,
-      input: 'oui'
     }
   }
 
@@ -17,13 +16,9 @@ class Introduction extends Component {
   }
 
   displayText() {
-    const { count } = this.state
     for (let i = 0; i <= text.length; i++) {
-
       setTimeout(_ => {
-        console.log(count)
         this.setState(prevState => {
-          console.log(prevState.count)
           return { 'count' : prevState.count + 1 }
         })
       }, i * 200 + (Math.random() * 100)) 
@@ -32,18 +27,13 @@ class Introduction extends Component {
   }
 
   render () {
-    const { count, input } = this.state
+    const { count } = this.state
 
     return (
-      <section className="introduction">
+      <section className="introduction crt">
         <Link to="home" className='introductionLink'>
             {text.substring(0, count)}
         </Link>
-        {/* {count === text.length + 1 && (
-          <div className="introductionInput">
-            {input}
-          </div>
-        )} */}
       </section>
     )
   }
